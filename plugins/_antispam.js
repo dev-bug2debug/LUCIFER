72d5f4cb11305e1e1cc42fda7c63292da0721526
dev-bug2debug <62154454+dev-bug2debug@users.noreply.github.com>
2021-11-15 15:07:35 +0530

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -5,8 +5,8 @@ handler.all = async function (m) {
     this.spam = this.spam ? this.spam : {}
     if (m.sender in this.spam) {
         this.spam[m.sender].count++
-        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-            if (this.spam[m.sender].count > 10) {
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 7) {
+            if (this.spam[m.sender].count > 7) {
                 //global.db.data.users[m.sender].banned = true
                 m.reply('*Do not Spam!!*')
             }
